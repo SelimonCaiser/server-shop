ca372ea35b6d4aa9f2b7973cca68ec8e346dbe60
SelimonCaiser <134937587+SelimonCaiser@users.noreply.github.com>
2023-08-13 15:42:03 +0300

    options

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://shop-carsfors.onrender.com'],
+    origin: ['http://localhost:3001', 'https://lifeperfect.vercel.app/'],
   });
 
   const config = new DocumentBuilder()
