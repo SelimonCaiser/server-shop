bcd3f33fd85dbebbb8940c4a35f241ffc7844a3b
SelimonCaiser <134937587+SelimonCaiser@users.noreply.github.com>
2023-08-13 15:49:10 +0300

    change main file

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
 
   app.enableCors({
     credentials: true,
-    origin: ['http://localhost:3001', 'https://lifeperfect.vercel.app/'],
+    origin: ['http://localhost:3001', 'https://lifeperfect.vercel.app'],
   });
 
   const config = new DocumentBuilder()
