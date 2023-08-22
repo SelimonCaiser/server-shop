4054ccb16d6466b7b1ec23da01d31d42ce97f500
SelimonCaiser <134937587+SelimonCaiser@users.noreply.github.com>
2023-08-22 14:11:15 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     credentials: true,
     origin: [
       'http://localhost:3001',
-      'https://shop-fullstack-client.vercel.app',
+      'https://lifeperfect.vercel.app',
     ],
   });
 
